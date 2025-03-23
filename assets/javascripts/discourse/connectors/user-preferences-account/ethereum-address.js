c24a3a4a87c9cc73e9ae37c5026c82b89a6bc3ed
codeiaks <cod3iaks@gmail.com>
2025-03-23 14:53:48 -0700

    replaced files

--- assets/javascripts/discourse/connectors/user-preferences-account/ethereum-address.js
+++ assets/javascripts/discourse/connectors/user-preferences-account/ethereum-address.js
@@ -1,9 +1,30 @@
 export default {
+  setupComponent(args, component) {
+    args.model.set(
+      "custom_fields.ethereum_address",
+      args.model.get("ethereum_address")
+    );
+
+    component.set("connectWallet", async function () {
+      console.log("Connect wallet clicked");
+      try {
+        if (typeof window.connectWallet === "function") {
+          const address = await window.connectWallet();
+          if (address) {
+            args.model.set("custom_fields.ethereum_address", address);
+            console.log("Wallet connected:", address);
+          }
+        } else {
+          console.error("window.connectWallet function not found");
+        }
+      } catch (error) {
+        console.error("Error connecting wallet:", error);
+      }
+      return false;
+    });
+  },
+
   shouldRender(args, component) {
     return component.siteSettings.discourse_ethereum_enabled;
   },
-  
-  setupComponent(args, component) {
-    args.model.set("custom_fields.ethereum_address", args.model.get("ethereum_address"));
-  }
 };
